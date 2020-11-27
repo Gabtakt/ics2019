f6619c4bba7c0d556c63f754c7715f37799eeafe
tracer-ics2019 <tracer@njuics.org>
2020-11-27 18:06:40 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:06:40 up  8:35,  1 user,  load average: 0.89, 0.40, 0.25 aa8a01a87ff62ec18f91d9ced7ab650bcc409b9a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -326,15 +326,12 @@ uint32_t eval(int p, int q, bool *success)
   switch (tokens[pos].type) {
     case TK_PLUS:
       val =  left_val + right_val;
-      printf("%d = %d + %d\n",val,left_val,right_val);
       break;
     case TK_MINUS:
       val = left_val - right_val;
-      printf("%d = %d - %d\n",val,left_val,right_val);
       break;
     case TK_MUL:
       val = left_val * right_val;
-      printf("%d = %d * %d\n",val,left_val,right_val);
       break;
     case TK_DIV:
       if (right_val == 0) {
@@ -343,7 +340,6 @@ uint32_t eval(int p, int q, bool *success)
         return 0;
       }
       val = left_val / right_val;
-      printf("%d = %d / %d\n",val,left_val,right_val);
       break;
     case TK_OR:
       val = left_val || right_val;
