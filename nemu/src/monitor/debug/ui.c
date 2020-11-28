c7a7e621d8cdf6f800419e4641eda9c0a9a3bce8
tracer-ics2019 <tracer@njuics.org>
2020-11-28 17:20:20 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:20:20 up  7:19,  1 user,  load average: 0.11, 0.31, 0.25 58a14e0de4218d56c10f93b651d543dc7d26b62b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -64,7 +64,7 @@ static int cmd_info(char *args) {
     isa_reg_display();
   }
   else if (strcmp(arg, "w") == 0) {
-    //TODO:
+    display_wp();
   }
   else {
     printf("Invalid parameter.\n");
