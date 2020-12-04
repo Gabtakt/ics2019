5cc4a7290aeed9f952d4013a3af5023d4fded81f
tracer-ics2019 <tracer@njuics.org>
2020-12-04 15:10:27 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:10:27 up 1 day,  1:17,  1 user,  load average: 0.86, 0.62, 0.55 be9786bbfdff00eba08f220a14c3d713751d4e5d

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -14,36 +14,40 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   /* pa2.2
    * 2020-12-4
+   * use register: t0, t1
    */
   switch (subcode & 0xe) {
     case CC_O:
-      rtl_get_OF(&s0);
+      rtl_get_OF(dest);
       break;
     case CC_B:
-      rtl_get_CF(&s0);
+      rtl_get_CF(dest);
       break;
     case CC_E:
-      rtl_get_ZF(&s0);
+      rtl_get_ZF(dest);
       break;
     case CC_BE:
-      rtl_get_CF(&s0);
-      rtl_get_ZF(&s1);
-      rtl_or(&s0, &s0, &s1);
+      rtl_get_CF(&t0);
+      rtl_get_ZF(&t1);
+      rtl_or(&t0, &t0, &t1);
+      *dest = t0;
       break;
     case CC_S:
-      rtl_get_SF(&s0);
+      rtl_get_SF(dest);
       break;
     case CC_L:
-      rtl_get_SF(&s0);
-      rtl_get_OF(&s1);
-      rtl_xor(&s0, &s0, &s1);
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(&t0, &t0, &t1);
+      *dest = t0;
       break;
     case CC_LE:
-      rtl_get_SF(&s0);
-      rtl_get_OF(&s1);
-      rtl_xor(&s0, &s0, &s1);
-      rtl_get_ZF(&s1);
-      rtl_or(&s0, &s0, &s1);
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(&t0, &t0, &t1);
+      rtl_get_ZF(&t1);
+      rtl_or(&t0, &t0, &t1);
+      *dest = t0;
       break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
