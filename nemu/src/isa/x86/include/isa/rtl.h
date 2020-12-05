6268a3169b9433b8c29f4b7820fbabbaffd9294e
tracer-ics2019 <tracer@njuics.org>
2020-12-05 14:30:14 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:30:14 up 1 day,  8:05,  1 user,  load average: 0.19, 0.13, 0.14 8953c3fba569f9faaf0278bac0c580770c6969a

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -26,93 +26,66 @@ static inline void rtl_sr(int r, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  /* pa2.1
-   * 2020-12-1
-   * use rtl_sm to access guest memory, to store 4 bytes data
-   */
-  // rtl_li(&s0, 4);
-  // rtl_sub(&reg_l(R_ESP), &reg_l(R_ESP), &s0);
-  reg_l(R_ESP) -=4;
+  /* PA2.1 */
+  reg_l(R_ESP) -= 4;
   rtl_sm(&reg_l(R_ESP), src1, 4);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  /* pa2.1
-   * 2020-12-2
-   */
+  /* PA2.1 */
   rtl_lm(dest, &reg_l(R_ESP), 4);
-  // rtl_li(&s0, 4);
-  // rtl_add(&reg_l(R_ESP), &reg_l(R_ESP), &s0);
-  reg_l(R_ESP) +=4;
+  reg_l(R_ESP) += 4;
 }
 
 static inline void rtl_is_sub_overflow(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1, const rtlreg_t* src2, int width) {
   // dest <- is_overflow(src1 - src2)
-  /* pa2.1
-   * 2020-12-2
-   * use registers: t0, t1
-   */
-  // OF will be set when x, y has different sign and x, (x-y) also has different sign
-  t0 = (*src1) >> (width * 8 - 1) & 0x1;
-  t1 = (*src2) >> (width * 8 - 1) & 0x1;
-  rtl_xor(&t1, &t0, &t1);
-  if (t1 == 0) {
-    *dest = 0;
-  }
-  else {
-    t1 = (*res) >> (width * 8 - 1) && 0x1;
-    rtl_xor(&t1, &t0, &t1);
-    *dest = t1;
+  /* PA2.1 */
+  t0 =  ((*src1)>>(8*width-1)) & 1;  
+  t1 =  ((*src2)>>(8*width-1)) & 1;  
+  
+  rtl_xor(&t1,&t0,&t1);
+  if(t1==0) *dest=0;
+  else{
+    t1=((*res)>>(8*width-1)) & 1;
+    rtl_xor(&t0,&t0,&t1);
+    *dest = t0;
   }
 }
 
 static inline void rtl_is_sub_carry(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1) {
   // dest <- is_carry(src1 - src2)
-  /* pa2.1
-   * 2020-12-2
-   */
+  /* PA2.1 */
   *dest = (*res > *src1);
 }
 
 static inline void rtl_is_add_overflow(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1, const rtlreg_t* src2, int width) {
   // dest <- is_overflow(src1 + src2)
-  /* pa2.2
-   * 2020-12-3
-   */
-  t0 = (*src1) >> (width * 8 - 1) & 0x1;
-  t1 = (*src2) >> (width * 8 - 1) & 0x1;
-  rtl_xor(&t1, &t0, &t1);
-  if (t1 == 0) {
-    *dest = 0;
-  }
-  else {
-    t1 = (*res) >> (width * 8 - 1) & 0x1;
-    rtl_xor(&t1, &t0, &t1);
-    *dest = t1;
+  t0 =  ((*src1)>>(8*width-1)) & 1;  
+  t1 =  ((*src2)>>(8*width-1)) & 1;  
+  
+  rtl_xor(&t1,&t0,&t1);
+  if(t1==0) *dest=0;
+  else{
+    t1=((*res)>>(8*width-1)) & 1;
+    rtl_xor(&t0,&t0,&t1);
+    *dest = t0;
   }
 }
 
 static inline void rtl_is_add_carry(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1) {
   // dest <- is_carry(src1 + src2)
-  /* pa2.1
-   * 2020-12-2
-   */
-  *dest = (*res < *src1);
+   *dest = (*res < *src1);
 }
 
-/* pa2.1
- * 2020-12-2
- * update the define
- */
 #define make_rtl_setget_eflags(f) \
   static inline void concat(rtl_set_, f) (const rtlreg_t* src) { \
-    reg_f(f) = *src; \
+    reg_f(f) = *src;  \
   } \
   static inline void concat(rtl_get_, f) (rtlreg_t* dest) { \
     *dest = reg_f(f); \
@@ -125,10 +98,7 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  /* pa2.1
-   * 2020-12-2
-   * use register:t0
-   */
+  /* PA2.1 */
   t0 = *result << (32 - width * 8);
   t0 = (t0 == 0);
   rtl_set_ZF(&t0);
@@ -136,12 +106,9 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  /* pa2.1
-   * 2020-12-2
-   * use register:t0
-   */
-  t0 = *result >> (width * 8 - 1);
-  t0 = (t0 & 0x1);
+  /* PA2.1 */
+  t0 = (*result) >> (8*width-1);
+  t0 = (t0 & 1);
   rtl_set_SF(&t0);
 }
 
