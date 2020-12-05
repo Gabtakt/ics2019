81b0252662506d96b92d002b27b14deb3969c1a5
tracer-ics2019 <tracer@njuics.org>
2020-12-05 13:11:57 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:11:57 up 1 day,  6:46,  1 user,  load average: 1.26, 0.67, 0.42 2f8674f6e25a87a5313dd787126e4fd6ae6f538

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -1,189 +1,93 @@
-// #include "cpu/exec.h"
-// #include "cc.h"
-
-// make_EHelper(test) {
-//   /* pa2.2
-//    * 2020-12-4
-//    * use registers: s0, s1
-//    */
-//   rtl_and(&s0, &id_dest->val, &id_src->val);
-//   // update ZF and SF
-//   rtl_update_ZFSF(&s0, id_dest->width);
-//   // set OF and CF to zero
-//   rtl_li(&s1, 0);
-//   rtl_set_OF(&s1);
-//   rtl_set_CF(&s1);
-
-//   print_asm_template2(test);
-// }
-
-// make_EHelper(and) {
-//   /* pa2.2
-//    * 2020-12-3
-//    * use registers: s0
-//    */
-//   rtl_and(&s0, &id_dest->val, &id_src->val);
-//   operand_write(id_dest, &s0);
-//   // update ZF and SF
-//   rtl_update_ZFSF(&s0, id_dest->width);
-//   // set OF and CF to zero
-//   rtl_li(&s1, 0);
-//   rtl_set_OF(&s1);
-//   rtl_set_CF(&s1);
-
-//   print_asm_template2(and);
-// }
-
-// make_EHelper(xor) {
-//   /* pa2.1
-//    * 2020-12-2
-//    * use registers: s0
-//    */
-//   rtl_xor(&s0, &id_dest->val, &id_src->val);
-//   operand_write(id_dest, &s0);
-//   // updat ZF and SF
-//   rtl_update_ZFSF(&s0, id_dest->width);
-//   print_asm_template2(xor);
-//   // set OF and CF to zero
-//   rtl_li(&s1, 0);
-//   rtl_set_OF(&s1);
-//   rtl_set_CF(&s1);
-
-//   print_asm_template2(xor);
-// }
-
-// make_EHelper(or) {
-//   /* pa2.2
-//    * 2020-12-4
-//    * use registers: 
-//    */
-//   rtl_or(&s0, &id_dest->val, &id_src->val);
-//   operand_write(id_dest, &s0);
-//   // updat ZF and SF
-//   rtl_update_ZFSF(&s0, id_dest->width);
-//   print_asm_template2(xor);
-//   // set OF and CF to zero
-//   rtl_li(&s1, 0);
-//   rtl_set_OF(&s1);
-//   rtl_set_CF(&s1);
-
-//   print_asm_template2(or);
-// }
-
-// make_EHelper(sar) {
-//   TODO();
-//   // unnecessary to update CF and OF in NEMU
-
-//   print_asm_template2(sar);
-// }
-
-// make_EHelper(shl) {
-//   TODO();
-//   // unnecessary to update CF and OF in NEMU
-
-//   print_asm_template2(shl);
-// }
-
-// make_EHelper(shr) {
-//   TODO();
-//   // unnecessary to update CF and OF in NEMU
-
-//   print_asm_template2(shr);
-// }
-
-// make_EHelper(setcc) {
-//   uint32_t cc = decinfo.opcode & 0xf;
-
-//   rtl_setcc(&s0, cc);
-//   operand_write(id_dest, &s0);
-
-//   print_asm("set%s %s", get_cc_name(cc), id_dest->str);
-// }
-
-// make_EHelper(not) {
-//   TODO();
-
-//   print_asm_template1(not);
-// }
 #include "cpu/exec.h"
 #include "cc.h"
 
 make_EHelper(test) {
+  /* pa2.2
+   * 2020-12-4
+   * use registers: s0, s1
+   */
   rtl_and(&s0, &id_dest->val, &id_src->val);
-	
+  // update ZF and SF
   rtl_update_ZFSF(&s0, id_dest->width);
-  s1 = 0;
-  rtl_set_CF(&s1);
+  // set OF and CF to zero
+  rtl_li(&s1, 0);
   rtl_set_OF(&s1);
+  rtl_set_CF(&s1);
 
   print_asm_template2(test);
 }
 
 make_EHelper(and) {
+  /* pa2.2
+   * 2020-12-3
+   * use registers: s0
+   */
   rtl_and(&s0, &id_dest->val, &id_src->val);
-  
-  rtl_update_ZFSF(&s0, id_dest -> width);
-  
-  // CF = 0, OF = 0
-  s1 = 0;
-  rtl_set_CF(&s1);
-  rtl_set_OF(&s1);
-  
   operand_write(id_dest, &s0);
-  
+  // update ZF and SF
+  rtl_update_ZFSF(&s0, id_dest->width);
+  // set OF and CF to zero
+  rtl_li(&s1, 0);
+  rtl_set_OF(&s1);
+  rtl_set_CF(&s1);
+
   print_asm_template2(and);
 }
 
 make_EHelper(xor) {
+  /* pa2.1
+   * 2020-12-2
+   * use registers: s0
+   */
   rtl_xor(&s0, &id_dest->val, &id_src->val);
-  
-  rtl_update_ZFSF(&s0, id_dest -> width);
-  
-  // CF = 0, OF = 0
-  s1 = 0;
-  rtl_set_CF(&s1);
-  rtl_set_OF(&s1);
-  
   operand_write(id_dest, &s0);
-  
+  // updat ZF and SF
+  rtl_update_ZFSF(&s0, id_dest->width);
+  print_asm_template2(xor);
+  // set OF and CF to zero
+  rtl_li(&s1, 0);
+  rtl_set_OF(&s1);
+  rtl_set_CF(&s1);
+
   print_asm_template2(xor);
 }
 
 make_EHelper(or) {
+  /* pa2.2
+   * 2020-12-4
+   * use registers: 
+   */
   rtl_or(&s0, &id_dest->val, &id_src->val);
-
-  rtl_update_ZFSF(&s0, id_dest -> width);
-  
-  // CF = 0, OF = 0
-  s1 = 0;
-  rtl_set_CF(&s1);
-  rtl_set_OF(&s1);
-  
   operand_write(id_dest, &s0);
+  // updat ZF and SF
+  rtl_update_ZFSF(&s0, id_dest->width);
+  print_asm_template2(xor);
+  // set OF and CF to zero
+  rtl_li(&s1, 0);
+  rtl_set_OF(&s1);
+  rtl_set_CF(&s1);
+
   print_asm_template2(or);
 }
 
 make_EHelper(sar) {
+  TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_sar(&s0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &s0);
-  rtl_update_ZFSF(&s0, id_dest->width);
+
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
+  TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_shl(&s0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &s0);
-  rtl_update_ZFSF(&s0, id_dest->width);
+
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
+  TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_shr(&s0, &id_dest->val, &id_src->val);
-  operand_write(id_dest, &s0);
-  rtl_update_ZFSF(&s0, id_dest->width);
+
   print_asm_template2(shr);
 }
 
@@ -197,24 +101,7 @@ make_EHelper(setcc) {
 }
 
 make_EHelper(not) {
-  rtl_not(&s0, &id_dest->val);
-  operand_write(id_dest, &s0);
-  
+  TODO();
+
   print_asm_template1(not);
 }
-
-make_EHelper(rol) {
-	s0 = id_dest -> val;
-	int i;
-	for(i = 0; i < id_src -> val; ++ i)	{
-		rtl_msb(&s1, &s0, id_dest->width);
-		s0 = s0 << 1;
-		s0 |= s1;
-		rtl_set_CF(&s1);	// unnecessary?
-	}
-	
-	operand_write(id_dest, &s0);
-	
-	print_asm_template2(rol);	
-}
-
