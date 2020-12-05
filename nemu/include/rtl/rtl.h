21a0ff7b62a98b505924f3f57dfb0907aba4b7d7
tracer-ics2019 <tracer@njuics.org>
2020-12-05 18:22:50 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:22:50 up 1 day, 11:57,  1 user,  load average: 0.32, 0.40, 0.42 37b5e7a51a052114921482afb226f99b557a3e5

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -132,7 +132,11 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-  TODO();
+  /* pa2.2
+   * 2020-12-5
+   * connect: x86/exec/logic.c makeDopHelper(not)
+   */
+  *dest = ~(*src1);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
