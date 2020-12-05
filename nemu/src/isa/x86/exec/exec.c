e58a30643effa9152e103ac412ed46e62cbeeb27
tracer-ics2019 <tracer@njuics.org>
2020-12-05 11:26:15 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:26:15 up 1 day,  5:01,  1 user,  load average: 1.54, 0.87, 0.54 8dff9023780c4e9e2c3d7eac4f58402173ca50ed

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -75,7 +75,7 @@ static OpcodeEntry opcode_table [512] = {
   /* 0x50 */	IDEX(r, push), IDEX(r, push), IDEX(r, push), IDEX(r, push),
   /* 0x54 */	EMPTY, IDEX(r, push), IDEX(r, push), IDEX(r, push),
   /* 0x58 */	IDEX(r, pop), IDEX(r, pop), IDEX(r, pop), IDEX(r, pop),
-  /* 0x5c */	EMPTY, EMPTY, IDEX(r, pop), IDEX(r, pop),
+  /* 0x5c */	EMPTY, IDEX(r, pop), IDEX(r, pop), IDEX(r, pop),
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EX(operand_size), EMPTY,
   /* 0x68 */	IDEX(push_SI, push), EMPTY, EMPTY, EMPTY,
