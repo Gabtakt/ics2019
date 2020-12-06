d8b3471ad151d52d803c4fdd06d0d700c61a0a83
tracer-ics2019 <tracer@njuics.org>
2020-12-06 19:01:21 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:01:21 up 1 day, 22:46,  1 user,  load average: 0.72, 0.57, 0.44 e42920ff453d653d288ba3471b6a6464e3fd1046

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -5,8 +5,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   /* pa2.2 */
   if (ref_r->eax != reg_l(R_EAX) || ref_r->ebx != reg_l(R_EBX) || ref_r->ecx != reg_l(R_ECX) || 
 		ref_r->edx != reg_l(R_EDX) || ref_r->ebp != reg_l(R_EBP) || ref_r->esi != reg_l(R_ESI) ||
-		ref_r->edi != reg_l(R_EDI) || ref_r->pc != cpu.pc ) 
-	{
+		ref_r->edi != reg_l(R_EDI) || ref_r->pc != cpu.pc ) {
 		return false;
 	}
 	return true;
