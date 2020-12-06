14568c1a1a23eaaab8509174156300b653d543c5
tracer-ics2019 <tracer@njuics.org>
2020-12-06 13:10:17 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:10:17 up 1 day, 18:34,  1 user,  load average: 0.55, 0.68, 0.55 ee5d6076ec5b294aea042ffce5f1ceb1745fa198

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -30,9 +30,8 @@ static inline void rtl_push(const rtlreg_t* src1) {
    * 2020-12-1
    * use rtl_sm to access guest memory, to store 4 bytes data
    */
-  rtl_li(&s0, 4);
-  rtl_sub(&reg_l(R_ESP), &reg_l(R_ESP), &s0);
-  //reg_l(R_ESP) -=4;
+  rtl_li(&t0, 4);
+  rtl_sub(&reg_l(R_ESP), &reg_l(R_ESP), &t0);
   rtl_sm(&reg_l(R_ESP), src1, 4);
 }
 
@@ -43,13 +42,8 @@ static inline void rtl_pop(rtlreg_t* dest) {
    * 2020-12-2
    */
   rtl_lm(dest, &reg_l(R_ESP), 4);
-  // rtl_li(&s0, 4);
-  // rtl_add(&reg_l(R_ESP), &reg_l(R_ESP), &s0);
   rtl_li(&t0, 4);
   rtl_add(&reg_l(R_ESP), &reg_l(R_ESP), &t0);
-  //rtl_mv(&reg_l(R_ESP), &s1);
-  //reg_l(R_ESP) +=4;
-  //printf ("%d\n",reg_l(R_ESP)==s1);
 }
 
 static inline void rtl_is_sub_overflow(rtlreg_t* dest,
