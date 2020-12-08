3064136fdd9af94d3df5634946284f5feb37bb5b
tracer-ics2019 <tracer@njuics.org>
2020-12-08 13:09:47 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:09:47 up  4:45,  1 user,  load average: 0.70, 0.32, 0.13 74093876a1f2ecaff3fb905d9e43c7241cefe6f3

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -56,10 +56,11 @@ int strcmp(const char* s1, const char* s2) {
     s1++;
     s2++;
   }
-  if ((*(unsigned char *)s1 - *(unsigned char *)s2) > 0) {
+  int ret = *(unsigned char *)s1 - *(unsigned char *)s2;
+  if (ret > 0) {
     return 1;
   }
-  if ((*(unsigned char *)s1 - *(unsigned char *)s2) == 0) {
+  if (ret == 0) {
     return 0;
   }
   return -1;
