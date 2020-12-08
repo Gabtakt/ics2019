85a5e3ca4f45a3356b159b0243b013339f45fafd
tracer-ics2019 <tracer@njuics.org>
2020-12-08 13:07:56 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:07:56 up  4:43,  1 user,  load average: 0.39, 0.20, 0.08 75c4a2a2536bf40d12edbc18c08406de76ae2e4a

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -51,30 +51,18 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  // assert(s1 != NULL && s2 != NULL);
-  // while (*s1 != '\0' && *s1 == *s2) {
-  //   s1++;
-  //   s2++;
-  // }
-  // if (*(unsigned char *)s1 > *(unsigned char *)s2) {
-  //   return 1;
-  // }
-  // if (*s1 == *s2) {
-  //   return 0;
-  // }
-  // return -1;
-    assert(s1 != NULL && s2 != NULL);
-  while(*s1 != '\0' && *s1 == *s2 ) {
-		s1 ++;
-		s2 ++;
-	}
-	
-	int ret = *(unsigned char *)s1 - *(unsigned char *)s2;
-	
-	if(ret > 0) ret = 1;
-	else if(ret < 0) ret = -1;
-	
-	return  ret;
+  assert(s1 != NULL && s2 != NULL);
+  while (*s1 != '\0' && *s1 == *s2) {
+    s1++;
+    s2++;
+  }
+  if ((*(unsigned char *)s1 - *(unsigned char *)s2) > 0) {
+    return 1;
+  }
+  if (*s1 == *s2) {
+    return 0;
+  }
+  return -1;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
