0dde781a0fc5030201811d80e0bfa271ea54878a
tracer-ics2019 <tracer@njuics.org>
2020-12-08 13:10:05 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:10:05 up  4:45,  1 user,  load average: 0.55, 0.30, 0.13 e2066a8f51f93974939506f8e52d447074b2c121

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -52,18 +52,17 @@ char* strcat(char* dst, const char* src) {
 
 int strcmp(const char* s1, const char* s2) {
   assert(s1 != NULL && s2 != NULL);
-  while (*s1 != '\0' && *s1 == *s2) {
-    s1++;
-    s2++;
-  }
-  int ret = *(unsigned char *)s1 - *(unsigned char *)s2;
-  if (ret > 0) {
-    return 1;
-  }
-  if (ret == 0) {
-    return 0;
-  }
-  return -1;
+  while(*s1 != '\0' && *s1 == *s2 ) {
+		s1 ++;
+		s2 ++;
+	}
+	
+	int ret = *(unsigned char *)s1 - *(unsigned char *)s2;
+	
+	if(ret > 0) ret = 1;
+	else if(ret < 0) ret = -1;
+	
+	return  ret;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
