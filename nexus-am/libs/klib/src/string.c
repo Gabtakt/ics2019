86ed6346dc3633ae57e252caab57f84131f2c3cc
U201714707-lbc <U20171707@hust.edu.cn>
2020-12-09 21:01:47 +0800

    pa2 done

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -51,9 +51,6 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
-  if (s1 == NULL || s2 == NULL) {
-    return 0;
-  }
   assert(s1 != NULL && s2 != NULL);
   while (*s1 != '\0' && *s1 == *s2) {
     s1++;
