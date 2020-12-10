398f7134e250a1fd62f72aaa9283c23222831650
tracer-ics2019 <tracer@njuics.org>
2020-12-10 15:37:42 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:37:42 up  6:02,  1 user,  load average: 0.94, 0.59, 0.40 fc5ab8e6f751f7881d5da0478348f58cc2c6830b

--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -1,10 +1,45 @@
 #ifndef __ARCH_H__
 #define __ARCH_H__
 
+/* pa3.1
+ * 2020-12-10
+ * Reorganize the Context:
+ *   ESP(paramater)
+ *   $0
+ *   EDI
+ *   ESI
+ *   EBP
+ *   ESP
+ *   EBX
+ *   EDX
+ *   ECX
+ *   EAX
+ *   IRQ
+ *   EIP(PC)
+ *   CS
+ *   EFLAGS
+ * NOTE: the oder depends on instruction 'int', it push elflags, cs, eip(pc),
+ * in nexus-am/am/src/x86/nemu/trap.S, first push irq, then call pusha to
+ *  push all registers.
+ */
 struct _Context {
-  uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
+  // uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
+  // struct _AddressSpace *as;
+  // int irq;
   struct _AddressSpace *as;
+  uintptr_t edi;
+  uintptr_t esi;
+  uintptr_t ebp;
+  uintptr_t esp;
+  uintptr_t ebx;
+  uintptr_t edx;
+  uintptr_t ecx;
+  uintptr_t eax;
   int irq;
+  uintptr_t eip;
+  uintptr_t cs;
+  uintptr_t eflags;
+
 };
 
 #define GPR1 eax
