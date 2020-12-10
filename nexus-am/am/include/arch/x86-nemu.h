3f442b381fe57c82ceebea01d05df3802103d357
tracer-ics2019 <tracer@njuics.org>
2020-12-10 15:47:11 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:47:11 up  6:11,  1 user,  load average: 0.84, 0.48, 0.40 ac391cf29adb25dfd8fc4be53312e2315f4e2475

--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -23,22 +23,22 @@
  *  push all registers.
  */
 struct _Context {
-  // uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
-  // struct _AddressSpace *as;
-  // int irq;
+  uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
   struct _AddressSpace *as;
-  uintptr_t edi;
-  uintptr_t esi;
-  uintptr_t ebp;
-  uintptr_t esp;
-  uintptr_t ebx;
-  uintptr_t edx;
-  uintptr_t ecx;
-  uintptr_t eax;
   int irq;
-  uintptr_t eip;
-  uintptr_t cs;
-  uintptr_t eflags;
+  // struct _AddressSpace *as;
+  // uintptr_t edi;
+  // uintptr_t esi;
+  // uintptr_t ebp;
+  // uintptr_t esp;
+  // uintptr_t ebx;
+  // uintptr_t edx;
+  // uintptr_t ecx;
+  // uintptr_t eax;
+  // int irq;
+  // uintptr_t eip;
+  // uintptr_t cs;
+  // uintptr_t eflags;
 
 };
 
