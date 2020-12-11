fd23b86697bd0eb250be164a92b3ffaf29db1c95
tracer-ics2019 <tracer@njuics.org>
2020-12-11 18:08:12 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:08:12 up 16:07,  1 user,  load average: 0.67, 0.64, 0.75 1a960484d6ac3a1c84ed09284d79a303d84ba80

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -19,7 +19,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   // read the elf header file from start
   ramdisk_read(&elf_header, 0x0, sizeof(Elf_Ehdr));
   size_t phoff = elf_header.e_phoff;
-  //uint64_t phoff = elf_header.e_phoff;
+  uint64_t phoff2 = elf_header.e_phoff;
+  printf("%d %d",phoff, phoff2);
   uint16_t e_phnum = elf_header.e_phnum;
   Elf_Phdr program_header;
   while (e_phnum--) {
