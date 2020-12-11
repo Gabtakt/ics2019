1601b3572eb5c703f5e020a3c202bbeffd51a2a0
tracer-ics2019 <tracer@njuics.org>
2020-12-11 17:50:26 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:50:26 up 15:49,  1 user,  load average: 1.59, 0.84, 0.59 d143a9ecdc5ba401363fbe494987aee44ec2f917

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,13 +29,23 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
      * Mem[p_vaddr + p_filesz - 1, p_vaddr] <- Mem[p_offset + p_filesz - 1, p_offset]
      * Mem[p_vaddr + p_filesz + p_memsz - 1, p_vaddr + p_filesz] <- 0
      */
-    if (program_header.p_type == PT_LOAD) {
+    // if (program_header.p_type == PT_LOAD) {
+    //   uint8_t buf[program_header.p_filesz];
+    //   ramdisk_read(&buf, program_header.p_offset, program_header.p_filesz);
+    //   memcpy((void *)program_header.p_vaddr, &buf, program_header.p_filesz);
+    //   memset((void *)program_header.p_vaddr + program_header.p_filesz, 0, program_header.p_memsz - program_header.p_filesz);
+    // }
+    // phoff += sizeof(Elf_Phdr);
+
+
+    ramdisk_read(&program_header,phoff,(size_t)sizeof(Elf_Phdr));
+    phoff+=sizeof(Elf_Phdr);
+    if(program_header.p_type == PT_LOAD){
       uint8_t buf[program_header.p_filesz];
-      ramdisk_read(&buf, program_header.p_offset, program_header.p_filesz);
-      memcpy((void *)program_header.p_vaddr, &buf, program_header.p_filesz);
-      memset((void *)program_header.p_vaddr + program_header.p_filesz, 0, program_header.p_memsz - program_header.p_filesz);
+      ramdisk_read(&buf,program_header.p_offset,program_header.p_filesz);
+      memcpy((void*)program_header.p_vaddr,&buf,program_header.p_filesz);
+      memset((void*)(program_header.p_vaddr+program_header.p_filesz),0,(program_header.p_memsz-program_header.p_filesz));
     }
-    phoff += sizeof(Elf_Phdr);
   }
   return elf_header.e_entry;
 }
