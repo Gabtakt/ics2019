97e84230c6448fbcc08d905dfc2d4751d0bcdc69
tracer-ics2019 <tracer@njuics.org>
2020-12-11 18:09:54 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:09:54 up 16:09,  1 user,  load average: 1.49, 0.85, 0.81 88c994b0532b9d9550c236aea6937a5831cd1481

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,7 +25,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Phdr program_header;
   while (e_phnum--) {
     // read all the program header file
-    ramdisk_read(&program_header, phoff, (size_t)sizeof(Elf_Phdr));
+    ramdisk_read(&program_header, (size_t)phoff2, (size_t)sizeof(Elf_Phdr));
     /* the segment should be loaded,
      * Mem[p_vaddr + p_filesz - 1, p_vaddr] <- Mem[p_offset + p_filesz - 1, p_offset]
      * Mem[p_vaddr + p_filesz + p_memsz - 1, p_vaddr + p_filesz] <- 0
