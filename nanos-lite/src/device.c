042212f48dc7954f76fd63718405c5dee105095f
tracer-ics2019 <tracer@njuics.org>
2020-12-13 22:23:10 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:23:10 up 1 day, 10:54,  1 user,  load average: 0.99, 0.83, 0.58 88965a189781920583ff95f0904e782d52003b30

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -57,7 +57,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   if (len > sizeof(dispinfo) - offset) {
     len = sizeof(dispinfo) - offset;
   }
-  strncpy((char *)buf, &dispinfo, len);
+  strncpy((char *)buf, dispinfo, len);
   return len;
 }
 
@@ -73,8 +73,9 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   // dx = offset % width;
   // draw_rect((uint32_t *)buf, dx, dy, pixels_num, 1);
   // return 0;
-  //Log("fb_write\n");
-  
+
+
+  //Log("fb_write\n"); 
   int x,y;
   offset = offset>>2;
   y = offset / screen_width();
