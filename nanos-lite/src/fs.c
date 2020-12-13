01772069b10361ffa4a454873254b898cb1d7ef7
tracer-ics2019 <tracer@njuics.org>
2020-12-13 17:15:04 +0800

    >  run U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:15:04 up 1 day,  5:46,  1 user,  load average: 1.23, 0.70, 0.48 f5bb174a6d085e36845029e9eab5ec30b4ebc345

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -3,10 +3,21 @@
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
+/* pa3.3
+ * 2020-12-13
+ */
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
 typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  // pa3.3
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -25,9 +36,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, invalid_read, invalid_write},
+  {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
@@ -35,4 +46,113 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+/* pa3.3
+ * 2020-12-13
+ * NOTE: nnaos-lite allows all user program w/r a file, so parameter 
+ * 'flags' and 'mode' can be ignored
+ */
+int fs_open(const char *pathname, int flags, int mode) {
+  int fd = 0;
+  for(; fd < NR_FILES; fd++) {
+    if (strcmp(file_table[fd].name, pathname) == 0) {
+      file_table[fd].open_offset = 0;
+      return fd;
+    }
+  }
+  panic("file %s not found.", pathname);
+  assert(0);
+  return -1;
+}
+
+/* pa3.3
+ * 2020-12-13
+ */
+size_t fs_read(int fd, void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+  // out of file size range, change it
+  if (file_table[fd].size > 0 && len > file_table[fd].size - file_table[fd].open_offset) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
+  assert(len >= 0);
+  size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
+  size_t ret = 0;
+  switch(fd) {
+    // ignore the next file operation
+    case FD_STDIN:
+    case FD_STDOUT:
+    case FD_STDERR:
+      break;
+    default:
+      ret = ramdisk_read(buf, offset, len);
+      file_table[fd].open_offset += ret;
+      break;
+  }
+  return ret;
+}
+
+/* pa3.3
+ * 2020-12-13
+ */
+size_t fs_write(int fd, const void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+  // out of file size range, change it
+  if (file_table[fd].size > 0 && len > file_table[fd].size - file_table[fd].open_offset) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
+  assert(len >= 0);
+  size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
+  size_t ret = 0;
+  switch(fd) {
+    // ignore the next file operation
+    case FD_STDIN:
+      break;
+    // call _putc() to output to the serial port
+    case FD_STDOUT:
+    case FD_STDERR:
+    {
+      int i = 0;
+      for ( ; i < len; i++) {
+        _putc(((char *)buf)[i]);
+      }
+      ret = len;
+      break;
+    }
+    default:
+      ret = ramdisk_write(buf, offset, len);
+      file_table[fd].open_offset += ret;
+      break;
+  }
+  return ret;
+}
+
+/* pa3.3
+ * 2020-12-13
+ */
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  assert(fd >= 0 && fd < NR_FILES);
+  size_t open_offset = file_table[fd].open_offset;
+  switch(whence) {
+    case SEEK_SET:
+      file_table[fd].open_offset = offset;
+      break;
+    case SEEK_CUR:
+      file_table[fd].open_offset += offset;
+      break;
+    case SEEK_END:
+      file_table[fd].open_offset = file_table[fd].size + offset;
+      break;
+    default:
+      panic("Unkown whence : %d", whence);
+  }
+  return file_table[fd].open_offset;
+}
+
+/* pa3.3
+ * 2020-12-13
+ * NOTE: PA do not need to close file, so always return 0
+ */
+int fs_close(int fd) {
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,34 +10,45 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   /* pa3.2
    * 2020-12-11
-   * in this step, parameter 'pcb' and 'filename' can be ignored
+   * FIXME: in this step, parameter 'pcb' and 'filename' can be ignored
+   */
+  /* pa3.3
+   * 2020-12-13
+   * FIX: use 'filename' in this step
    */
   Elf_Ehdr elf_header;
+  int fd = fs_open(filename, 0, 0);
+  assert(fd != -1);
   // read the elf header file from start
-  ramdisk_read(&elf_header, 0x0, sizeof(Elf_Ehdr));
-  size_t phoff = elf_header.e_phoff;
+  fs_read(fd, &elf_header, sizeof(Elf_Ehdr));
+  size_t open_offset = fs_lseek(fd, elf_header.e_phoff, SEEK_SET);
+
   uint16_t e_phnum = elf_header.e_phnum;
   Elf_Phdr program_header;
+  // read all the program header file
   while (e_phnum--) {
-    // read all the program header file
-    ramdisk_read(&program_header, phoff, sizeof(Elf_Phdr));
-    /* the segment should be loaded,
+    open_offset += fs_read(fd, &program_header, sizeof(Elf_Phdr));
+    assert(fd != -1);
+    /* the segment should be loaded.
      * Mem[p_vaddr + p_filesz - 1, p_vaddr] <- Mem[p_offset + p_filesz - 1, p_offset]
      * Mem[p_vaddr + p_filesz + p_memsz - 1, p_vaddr + p_filesz] <- 0
      */
     if (program_header.p_type == PT_LOAD) {
-      uint8_t buf[program_header.p_filesz];
-      ramdisk_read(&buf, program_header.p_offset, program_header.p_filesz);
-      memcpy((void *)program_header.p_vaddr, &buf, program_header.p_filesz);
+      fs_lseek(fd, program_header.p_offset, SEEK_SET);
+      fs_read(fd, (void *)program_header.p_vaddr, program_header.p_filesz);
       memset((void *)program_header.p_vaddr + program_header.p_filesz, 0, program_header.p_memsz - program_header.p_filesz);
     }
-    phoff += sizeof(Elf_Phdr);
+    fs_lseek(fd, open_offset, SEEK_SET);
   }
+  fs_close(fd);
   return elf_header.e_entry;
 }
 
