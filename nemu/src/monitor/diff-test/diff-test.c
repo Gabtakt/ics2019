17714a60d2e152e7ad6f15cec1f6659c6b526c28
tracer-ics2019 <tracer@njuics.org>
2020-12-14 20:21:09 +0800

    >  compile U201714707 hust Linux hust-desktop 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:21:09 up 2 days, 30 min,  1 user,  load average: 1.68, 0.64, 0.36 e389c07c482d31ed89dee43891434b9674cc8165

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -92,7 +92,6 @@ void init_difftest(char *ref_so_file, long img_size) {
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
-  printf("call check here");
   if (!isa_difftest_checkregs(ref, pc)) {
     extern void isa_reg_display(void);
     isa_reg_display();
